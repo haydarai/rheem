0bc30ec87a4b75e7e51229dd54bef8e8b146d42d
atroudi <atroudi@qf.org.qf>
2018-01-26 18:00:15 +0300

    Integrating Mlearned costs with lossless operator pruning! [Adding ml loading file ]

---
+++ rheem-core/src/main/resources/loadModel.py
@@ -0,0 +1,76 @@
+import numpy
+import sys
+from numpy import loadtxt
+import pickle
+import os
+from pathlib import Path
+
+def main():
+    #inputFile = loadtxt("planVectorsSGD2-kmeans-simword-opportuneWordcount.txt", comments="#", delimiter=" ", unpack=False)
+
+    currentDirPath =os.path.dirname(os.path.realpath(__file__))
+
+    dirPath = str(Path.home())
+
+    if (len(sys.argv)>=2):
+        inputFile = loadtxt(sys.argv[1], comments="#", delimiter=" ", unpack=False)
+    else:
+        inputFile = loadtxt(os.path.join(dirPath,".rheem","mlModelVectors.txt"), comments="#", delimiter=" ", unpack=False)
+
+    #size = 146;
+    #start = 13;
+    #size = 213
+    size=251
+    start = 0
+    x_test = inputFile[:,0:size]
+    y_test = inputFile[start:,size]
+
+    # x_train = inputFile[:,0:size]
+    # y_train = inputFile[:,size]
+    #
+    # x_test = inputFile[:,0:size]
+    # y_test = inputFile[:,size]
+
+
+
+    # load the model from disk
+    filename = os.path.join(currentDirPath,"ForestModel.sav")
+    regr = pickle.load(open(filename, 'rb'))
+
+
+
+    # fix random seed for reproducibility
+    seed = 7
+    numpy.random.seed(seed)
+    #kfold = KFold(n_splits=10, random_state=seed)
+    #results = cross_val_score(regr, x_train, y_train, cv=kfold)
+    #accuracy_score(prediction,y_train)
+    #print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+    prediction = regr.predict(x_test)
+
+    # for num in range(1,min([34,len(x_test)])):
+    #     if num % 2 == 0:
+    #         print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in java : " + str(
+    #             prediction[num]) + "(real " + str(y_test[num]) + ")")
+    #     else:
+    #         print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in spark : " + str(
+    #             prediction[num]) + "(real " + str(y_test[num]) + ")")
+
+    # print results to text
+    if (len(sys.argv) >= 3):
+        saveLocation = loadtxt(sys.argv[2], comments="#", delimiter=" ", unpack=False)
+    else:
+        saveLocation = os.path.join(dirPath,".rheem","estimates.txt")
+
+    # delete first
+    if(os._exists(saveLocation)):
+        os.remove(saveLocation)
+    text_file = open(saveLocation, "w")
+    for num in range(1, prediction.size):
+        text_file.write("%d" % prediction[num])
+        text_file.write("\n")
+    text_file.close()
+    print("estimation done!")
+
+if __name__ == "__main__":
+   main()
